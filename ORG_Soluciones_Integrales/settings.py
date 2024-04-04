edc57df8819021321982ac13224b000e3e6431a9
SamuelStinson02 <samuelstinson02@gmail.com>
2024-04-04 15:06:34 -0400

    First Commit

--- ORG_Soluciones_Integrales/settings.py
+++ ORG_Soluciones_Integrales/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -73,10 +74,11 @@ WSGI_APPLICATION = 'ORG_Soluciones_Integrales.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgres://postgres.pzmjtscszjuiudnqqysr:xdcIwK0hpu6ct3pv@aws-0-us-west-1.pooler.supabase.com:5432/postgres',
+        conn_max_age=600
+    )
 }
 
 # Password validation
