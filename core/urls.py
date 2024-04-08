f5b30b62a2a0d0df2f0c17c7ba162990e0fb6686
SamuelStinson02 <samuelstinson02@gmail.com>
2024-04-08 15:34:11 -0400

    First Commit

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from .views import RobotsTxtView
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('robots.txt', RobotsTxtView.as_view(content_type="text/plain"), name="robots"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 
 # Create your views here.
 def home(request):
     return render(request, 'web/index.html')
+
+
+class RobotsTxtView(TemplateView):
+    template_name = "robots.txt"
